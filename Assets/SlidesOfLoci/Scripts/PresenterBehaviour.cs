6f44ca34fdeadb6334cdd33db1307ef79c824f26
florian <florian.mehnert@gmail.com>
2025-01-30 14:00:10 +0100

    fix runtime not able to switch issues

--- Assets/SlidesOfLoci/Scripts/PresenterBehaviour.cs
+++ Assets/SlidesOfLoci/Scripts/PresenterBehaviour.cs
@@ -1,9 +1,9 @@
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [Serializable]
 public class PresenterBehaviour : MonoBehaviour
@@ -12,166 +12,147 @@ public class PresenterBehaviour : MonoBehaviour
     public List<GameObject> slides = new();
     private GameObject[] _sections;
     private int _slidesNum;
+    private Camera _mainCamera;
 
-    private int _lastSlideIndex = -1;
-
-    public virtual void Start()
+    private void Start()
     {
-#if UNITY_EDITOR
+        _mainCamera = GetComponent<Camera>();
+        
+        // Adjust camera FOV for aspect ratio
+        if (_mainCamera != null)
+        {
+            _mainCamera.fieldOfView = _mainCamera.fieldOfView * 16f / 9f * (1f / _mainCamera.aspect);
+        }
+
+        InitializeSlides();
+    }
+
+    private void InitializeSlides()
+    {
+        _sections = GameObject.FindGameObjectsWithTag("Section");
         _slidesNum = GameObject.FindGameObjectsWithTag("Slide").Length;
 
+        // Clear existing slides
+        slides.Clear();
+
+        #if UNITY_EDITOR
+        // In editor, simply add all slides
         foreach (GameObject slide in GameObject.FindGameObjectsWithTag("Slide"))
         {
             slides.Add(slide);
+            var slideCamera = slide.GetComponent<Camera>();
+            if (slideCamera != null) slideCamera.enabled = false;
         }
-
-        EditorApplication.update += EditorUpdate;
-#else
-        GetComponent<Camera>().fieldOfView = GetComponent<Camera>().fieldOfView * 16 / 9 * 1 / GetComponent<Camera>().aspect;
-        /*
-        We use two tags to reference and organize slides (scenes) : Slide and Section
-
-        Slides should be organized in sections. Sections are read ordered by how they appear in the
-        hierarchy and then we iterate through slides inside each sectino and get them in the order they
-        appear in the editor window's hierarchy
-
-        You have to create at least 1 section and 1 slide for a slideshow
-           */
-        _slidesNum = GameObject.FindGameObjectsWithTag("Slide").Length;
-        _sections = GameObject.FindGameObjectsWithTag("Section");
-
+        #else
+        // In runtime, organize slides by sections
         Array.Sort(_sections, Compare);
-        var i = 0;
-        while (i < _sections.Length)
+        
+        foreach (var section in _sections)
         {
-            foreach (Transform child in _sections[i].transform)
+            foreach (Transform child in section.transform)
             {
-                if (child.gameObject.tag != "Slide") continue;
-                child.gameObject.GetComponent<Camera>().enabled = false; // disable all cameras on start
-                slides.Add(child.gameObject);
+                if (child.CompareTag("Slide"))
+                {
+                    slides.Add(child.gameObject);
+                    var slideCamera = child.GetComponent<Camera>();
+                    if (slideCamera != null) slideCamera.enabled = false;
+                }
             }
-            i++;
         }
-#endif
+        #endif
+
+        // Enable first slide's settings
+        if (slides.Count > 0)
+        {
+            UpdateSlideSettings(slides[0]);
+        }
     }
 
-    public virtual void Update()
+    private void Update()
     {
-#if UNITY_EDITOR
         HandleInput();
-        UpdateCameraTransform();
-#else
-        /*
-        You can use the LEFT and RIGHT keys to navigate between slides
-        */
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (currSlide < _slidesNum - 1)
-            {
-                currSlide += 1; //slide number has to be in the range of slides
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (currSlide < _slidesNum - 1)
-            {
-                currSlide += 1; //slide number has to be in the range of slides
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (currSlide > 0)
-            {
-                currSlide -= 1; //slide number has to be in the range of slides
-            }
-        }
-
-        /*
-        Slide transitions:
-        Transition is either 0 or some positive float. If zero no animation happens, if the number is bigger then zero, then
-        the transition time attribute of the current slide will control the speed of the transition.
-
-        The camera that goes through the scenes will inherit some attributes of the camera of the given slides.
-        Currently these are:
-            -  clipping planes
-            -  skybox
-
-           */
-        RenderSettings.skybox = ((Skybox)slides[currSlide].GetComponent<Camera>().GetComponent(typeof(Skybox))).material;
-        GetComponent<Camera>().nearClipPlane = slides[currSlide].GetComponent<Camera>().nearClipPlane;
-        GetComponent<Camera>().farClipPlane = slides[currSlide].GetComponent<Camera>().farClipPlane;
-        var transitionTime = ((Attributes)slides[currSlide].GetComponent(typeof(Attributes))).transitionTime;
-        if (transitionTime > 0)
-        {
-            transform.position = Vector3.Lerp(transform.position, slides[currSlide].transform.position, Time.deltaTime / transitionTime * 1.15f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, slides[currSlide].transform.rotation, Time.deltaTime / transitionTime);
-        }
-        else
-        {
-            transform.position = slides[currSlide].transform.position;
-            transform.rotation = slides[currSlide].transform.rotation;
-        }
-#endif
+        UpdateCurrentSlide();
     }
 
     private void HandleInput()
     {
+        bool shouldAdvance = Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Space);
+        bool shouldReverse = Input.GetKeyDown(KeyCode.LeftArrow);
+
+        if (shouldAdvance && currSlide < _slidesNum - 1)
         {
-            if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Space))
+            currSlide++;
+        }
+        else if (shouldReverse && currSlide > 0)
+        {
+            currSlide--;
+        }
+    }
+
+    private void UpdateCurrentSlide()
+    {
+        if (slides.Count == 0 || currSlide < 0 || currSlide >= slides.Count) return;
+
+        var currentSlideObj = slides[currSlide];
+        var slideCamera = currentSlideObj.GetComponent<Camera>();
+        var slideAttributes = currentSlideObj.GetComponent<Attributes>();
+
+        if (slideCamera != null && _mainCamera != null)
+        {
+            // Update camera settings
+            UpdateSlideSettings(currentSlideObj);
+
+            // Handle transitions
+            float transitionTime = slideAttributes != null ? slideAttributes.transitionTime : 0f;
+            
+            if (transitionTime > 0)
             {
-                if (currSlide < _slidesNum - 1) currSlide++;
+                // Smooth transition
+                transform.position = Vector3.Lerp(transform.position, slideCamera.transform.position, 
+                    Time.deltaTime / transitionTime * 1.15f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, slideCamera.transform.rotation, 
+                    Time.deltaTime / transitionTime);
             }
-
-            if (!Input.GetKeyDown(KeyCode.LeftArrow) || !Input.GetKey(KeyCode.LeftAlt)) return;
+            else
+            {
+                // Instant transition
+                transform.position = slideCamera.transform.position;
+                transform.rotation = slideCamera.transform.rotation;
+            }
         }
-        if (currSlide > 0) currSlide--;
-    }
 
-    private void UpdateCameraTransform()
-    {
-#if UNITY_EDITOR
-        if (slides.Count == 0 || currSlide < 0 || currSlide >= slides.Count) return;
-
-        var targetSlide = slides[currSlide];
-        var targetCam = targetSlide.GetComponent<Camera>();
-
-        if (targetCam)
+        #if UNITY_EDITOR
+        // Update Scene view camera in editor
+        if (!Application.isPlaying && SceneView.lastActiveSceneView != null)
         {
-            Camera mainCam = GetComponent<Camera>();
-            mainCam.transform.position = targetCam.transform.position;
-            mainCam.transform.rotation = targetCam.transform.rotation;
-        }
-#endif
-    }
-
-    private void EditorUpdate()
-    {
-        if (Application.isPlaying || _lastSlideIndex == currSlide) return;
-        UpdateSceneViewCamera();
-        _lastSlideIndex = currSlide;
-    }
-
-    private void UpdateSceneViewCamera()
-    {
-        if (slides.Count == 0 || currSlide < 0 || currSlide >= slides.Count) return;
-
-        var targetSlide = slides[currSlide];
-        var slideCam = targetSlide.GetComponent<Camera>();
-#if UNITY_EDITOR
-        if (slideCam && SceneView.lastActiveSceneView != null)
-        {
-            SceneView.lastActiveSceneView.pivot = slideCam.transform.position;
-            SceneView.lastActiveSceneView.rotation = slideCam.transform.rotation;
+            SceneView.lastActiveSceneView.pivot = transform.position;
+            SceneView.lastActiveSceneView.rotation = transform.rotation;
             SceneView.lastActiveSceneView.Repaint();
         }
-#endif
+        #endif
+    }
+
+    private void UpdateSlideSettings(GameObject slide)
+    {
+        if (_mainCamera == null) return;
+        
+        var slideCamera = slide.GetComponent<Camera>();
+        if (slideCamera == null) return;
+
+        // Update camera settings
+        _mainCamera.nearClipPlane = slideCamera.nearClipPlane;
+        _mainCamera.farClipPlane = slideCamera.farClipPlane;
+
+        // Update skybox if available
+        var slideSkybox = slide.GetComponent<Skybox>();
+        if (slideSkybox != null && slideSkybox.material != null)
+        {
+            RenderSettings.skybox = slideSkybox.material;
+        }
     }
 
     public virtual int Compare(GameObject go1, GameObject go2)
     {
-        // compare function to sort the sections
         return string.Compare(go1.name, go2.name, StringComparison.Ordinal);
     }
 
@@ -179,14 +160,7 @@ public class PresenterBehaviour : MonoBehaviour
     {
         if (!Application.isPlaying)
         {
-            UpdateSceneViewCamera();
+            UpdateCurrentSlide();
         }
-    }
-
-    private void OnDestroy()
-    {
-#if UNITY_EDITOR
-        EditorApplication.update -= EditorUpdate;
-#endif
     }
 }
